81f3ef2f4a3a44c03312d5de44a4157ecdd65a14
eneun <wtcat@naver.com>
2019-07-29 19:25:52 +0900

    commit

--- newapp/views.py
+++ newapp/views.py
@@ -5,6 +5,7 @@ from django.core.files.base import ContentFile
 from django.shortcuts import render
 from google.cloud import vision
 from newapp.models import history
+from google.cloud.vision import types
 
 # Create your views here.
 def index(request):
@@ -12,15 +13,19 @@ def index(request):
 
 def info(request):
     #vision api code which also saves data to db.
-    vision_client = vision.Client()
+    vision_client = vision.ImageAnnotatorClient()
     myfile = request.FILES["pic"]
     fs = FileSystemStorage()
     filename = fs.save(myfile.name, myfile)
     file_name = fs.url(filename)
     with io.open(file_name, "rb") as image_file:
         content = image_file.read()
-        image = vision_client.image(content=content)
-    labels = image.detect_labels()
+    image = types.Image(content=content)
+
+    # 추가
+    response = vision_client.label_detection(image=image)
+    labels = response.label_annotations
+
     label_data=""
     for label in labels:
         label_data=label_data+label.description+"="+str(label.score)+","
